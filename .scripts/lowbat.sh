0d246a84e067a29695264f818c17656ddeb0ea1a
LukasP46 <spjs01pro@gmail.com>
2021-12-18 21:06:02 +0100

    updates

--- .scripts/lowbat.sh
+++ .scripts/lowbat.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
+acpi -b | grep "Battery 0" | awk -F'[,:%]' '{print $2, $3}' | {
 	read -r status capacity
 	if [ "$status" = Discharging -a "$capacity" -lt 2 ]; then
-		logger "Critical battery threshold - poweroff!"
-		systemctl poweroff
+		logger -p crit "Critical battery threshold - poweroff!"
+		#systemctl poweroff
 	fi
 	if [ "$status" = Discharging -a "$capacity" -lt 5 ]; then
-		logger "Critical battery threshold - suspend now"
-		systemctl suspend
+		logger -p crit "Critical battery threshold - suspend now"
+		#systemctl suspend
 	fi
 }
